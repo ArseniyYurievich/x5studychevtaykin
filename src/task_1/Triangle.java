4e60ff414346e0d8a297c77d16c8afa9208d7a0a
Arseniy <chevtaikin@mail.ru>
2020-09-23 08:50:14 +0300

    code review 1

--- src/task_1/Triangle.java
+++ src/task_1/Triangle.java
@@ -4,13 +4,13 @@ public class Triangle implements Shape {
     private double base;
     private double height;
 
-    @Override
-    public double square() {
-        return 0.5d * base * height;
-    }
-
     public Triangle(double base, double height) {
         this.base = base;
         this.height = height;
+    }
+
+    @Override
+    public double square() {
+        return 0.5d * base * height;
     }
 }
